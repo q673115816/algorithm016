90bcf51fed1c3dc55944173317f8a27963a7834e
林锦骏 <673115816@qq.com>
2020-09-23 18:06:54 +0800

    3

---
+++ Week_02/2/144.二叉树的前序遍历.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if(!root) return []
+    let result = []
+    let stack = [root]
+    while(stack.length) {
+        let node = stack.pop()
+        result.push(node.val)
+        if(node.right) stack.push(node.right)
+        if(node.left) stack.push(node.left)
+    }
+    return result
+};
+// var preorderTraversal = function(root) {
+//     let result = []
+//     dfs(root, result)
+//     return result
+// };
+
+// function dfs(node, result) {
+//     if(node) {
+//         result.push(node.val)
+//         if(node.left) dfs(node.left, result)
+//         if(node.right) dfs(node.right, result)
+//     }
+// }
+// @lc code=end
+
---
+++ Week_02/3/94.二叉树的中序遍历.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root) return []
+    let result = []
+    let stack = []
+    let node = root
+    while(stack.length || node) {
+        if(node) {
+            stack.push(node)
+            node = node.left
+        } else {
+            node = stack.pop()
+            result.push(node.val)
+            node = node.right
+        }
+    }
+    return result
+};
+// var inorderTraversal = function(root) {
+//     let result = []
+//     dfs(root, result)
+//     return result
+// };
+
+// function dfs(node, result) {
+//     if(node) {
+//         if(node.left) dfs(node.left, result)
+//         result.push(node.val)
+//         if(node.right) dfs(node.right, result)
+//     }
+// }
+// @lc code=end
+
