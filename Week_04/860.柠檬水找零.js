efe0b4479ae8e400f81cb474ae6da05b088389a0
林锦骏 <673115816@qq.com>
2020-10-12 00:12:02 +0800

    home4

---
+++ Week_04/860.柠檬水找零.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=860 lang=javascript
+ *
+ * [860] 柠檬水找零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0
+    let ten = 0
+    for(let bill of bills) {
+        if(bill === 5) {
+            five++
+        } else if(bill === 10) {
+            if(five === 0) return false
+            five--
+            ten++
+        } else if(bill === 20) {
+            if(ten > 0 && five > 0) {
+                ten--
+                five--
+            } else if(five > 2) {
+                five -= 3
+            } else {
+                return false
+            }
+        }
+    }
+    return true
+};
+// @lc code=end
+
